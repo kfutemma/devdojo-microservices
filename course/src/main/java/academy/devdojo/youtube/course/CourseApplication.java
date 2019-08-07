e1f3bb14bcdf17f7cf428c9609e3949d34d45ac7
Kaique de Paula Futemma <kaiquepf@cit>
2019-08-07 11:30:43 -0300

    Criação do modulo discovery (Eureka)

--- course/src/main/java/academy/devdojo/youtube/course/CourseApplication.java
+++ course/src/main/java/academy/devdojo/youtube/course/CourseApplication.java
@@ -6,12 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan({"academy.devdojo.youtube.core.model"})
-@EnableJpaRepositories({"academy.devdojo.youtube.core.repository"})
+@EntityScan({ "academy.devdojo.youtube.core.model" })
+@EnableJpaRepositories({ "academy.devdojo.youtube.core.repository" })
 public class CourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseApplication.class, args);
     }
-
 }
